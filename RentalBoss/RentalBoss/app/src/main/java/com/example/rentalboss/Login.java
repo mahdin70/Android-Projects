638677d739782fa1234c40a95a8ab251221de980
Mukit Mahdin <88025578+mahdin70@users.noreply.github.com>
2022-06-22 21:04:05 +0600

    initial

---
+++ RentalBoss/RentalBoss/app/src/main/java/com/example/rentalboss/Login.java
@@ -0,0 +1,149 @@
+package com.example.rentalboss;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+public class Login extends AppCompatActivity
+{
+
+    Button call_SignUp;
+    Button login_User;
+    ImageView logo_image;
+    TextView logo_text,signin_text;
+    TextInputLayout username_text,password_text;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_login);
+
+        call_SignUp = findViewById(R.id.call_SignUp);
+        logo_image = findViewById(R.id.logo_image);
+        logo_text = findViewById(R.id.logo_text);
+        login_User = findViewById(R.id.login_User);
+        signin_text = findViewById(R.id.signin_text);
+        username_text = findViewById(R.id.username_text);
+        password_text = findViewById(R.id.password_text);
+
+
+        call_SignUp.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(Login.this,SignUp.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private boolean validateUsername(){
+        String val = username_text.getEditText().getText().toString();
+        if(val.isEmpty())
+        {
+            username_text.setError("This field cannot be left empty");
+            return false;
+        }
+        else
+        {
+            username_text.setError(null);
+            username_text.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    private boolean validatePassword() {
+        String val = password_text.getEditText().getText().toString();
+        if(val.isEmpty())
+        {
+            password_text.setError("This field cannot be left empty");
+            return false;
+        }
+        else
+        {
+            password_text.setError(null);
+            password_text.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    public void loginUser(View view){
+        if(!validateUsername() | !validatePassword())
+        {
+            return;
+        }
+        else
+        {
+            isUser();
+        }
+    }
+
+    private void isUser() {
+        String userEnteredUserName = username_text.getEditText().getText().toString().trim();
+        String userEnteredPassword = password_text.getEditText().getText().toString().trim();
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("users");
+        Query checkUser = reference.orderByChild("username").equalTo(userEnteredUserName);
+
+        checkUser.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists()){
+
+                    username_text.setError(null);
+                    username_text.setErrorEnabled(false);
+
+                    String passwordfromDB = dataSnapshot.child(userEnteredUserName).child("password").getValue(String.class);
+                    if(passwordfromDB.equals(userEnteredPassword)){
+                        username_text.setError(null);
+                        username_text.setErrorEnabled(false);
+
+                        String namefromDB = dataSnapshot.child(userEnteredUserName).child("name").getValue(String.class);
+                        String emailfromDB = dataSnapshot.child(userEnteredUserName).child("email").getValue(String.class);
+                        String phoneNofromDB = dataSnapshot.child(userEnteredUserName).child("phoneNo").getValue(String.class);
+                        String usernamefromDB = dataSnapshot.child(userEnteredUserName).child("username").getValue(String.class);
+
+                        Intent intent = new Intent(getApplicationContext(),UserProfile.class);
+                        intent.putExtra("name",namefromDB);
+                        intent.putExtra("email",emailfromDB);
+                        intent.putExtra("phoneNo",phoneNofromDB);
+                        intent.putExtra("username",usernamefromDB);
+                        intent.putExtra("password",passwordfromDB);
+
+                        startActivity(intent);
+                    }
+                    else{
+                        password_text.setError("Wrong Password");
+                        password_text.requestFocus();
+                    }
+                }
+                else{
+                    username_text.setError("No Such User Exists");
+                    username_text.requestFocus();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+}
---
+++ RentalBoss/RentalBoss/app/src/main/java/com/example/rentalboss/SignUp.java
@@ -0,0 +1,181 @@
+package com.example.rentalboss;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+
+public class SignUp extends AppCompatActivity
+{
+    TextInputLayout reg_name,reg_username,reg_email,reg_password,reg_phoneNo;
+    Button reg_btn,reg_loginbtn;
+
+    FirebaseDatabase rootNode;
+    DatabaseReference reference;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        reg_name = findViewById(R.id.reg_name);
+        reg_username = findViewById(R.id.reg_username);
+        reg_email = findViewById(R.id.reg_email);
+        reg_password = findViewById(R.id.reg_password);
+        reg_phoneNo = findViewById(R.id.reg_phoneNo);
+        reg_btn = findViewById(R.id.reg_btn);
+        reg_loginbtn = findViewById(R.id.reg_loginbtn);
+
+        reg_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!validateName()| !validateUserName() | !validateEmail() | !validatePhoneNo() | !validatePassword())
+                {
+                    return;
+                }
+
+                rootNode = FirebaseDatabase.getInstance();
+                reference = rootNode.getReference("users");
+
+                String name = reg_name.getEditText().getText().toString();
+                String username = reg_username.getEditText().getText().toString();
+                String email = reg_email.getEditText().getText().toString();
+                String phoneNo = reg_phoneNo.getEditText().getText().toString();
+                String password = reg_password.getEditText().getText().toString();
+
+                UserHelperClass helperClass = new UserHelperClass(name,username,email,phoneNo,password);
+                reference.child(username).setValue(helperClass);
+
+                Toast.makeText(SignUp.this, "You have been registered to Rental Boss", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        reg_loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignUp.this,Login.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+   private boolean validateName()
+    {
+        String val = reg_name.getEditText().getText().toString();
+
+        if(val.isEmpty()){
+            reg_name.setError("This field cannot be left empty");
+            return false;
+        }
+        else{
+            reg_name.setError(null);
+            reg_name.setErrorEnabled(false);
+            return true;
+        }
+    }
+    private boolean validateUserName()
+    {
+        String val = reg_username.getEditText().getText().toString();
+        String noWhiteSpace = "\\A\\w{4,20}\\z";
+
+        if(val.isEmpty())
+        {
+            reg_username.setError("This field cannot be left empty");
+            return false;
+        }
+        else if(val.length() >=15)
+        {
+            reg_username.setError("Username is too long");
+            return false;
+        }
+        else if(!val.matches(noWhiteSpace))
+        {
+            reg_username.setError("Whitespaces and special characters are not allowed");
+            return false;
+        }
+        else
+        {
+            reg_username.setError(null);
+            reg_username.setErrorEnabled(false);
+            return true;
+        }
+    }
+    private boolean validateEmail()
+    {
+        String val = reg_email.getEditText().getText().toString();
+        String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
+        if(val.isEmpty())
+        {
+            reg_email.setError("This field cannot be left empty");
+            return false;
+        }
+        else if(!val.matches(emailPattern))
+        {
+            reg_email.setError("Invalid Email Address");
+            return false;
+        }
+        else
+        {
+            reg_email.setError(null);
+            reg_email.setErrorEnabled(false);
+            return true;
+        }
+    }
+    private boolean validatePhoneNo()
+    {
+        String val = reg_phoneNo.getEditText().getText().toString();
+
+        if(val.isEmpty())
+        {
+            reg_phoneNo.setError("This field cannot be left empty");
+            return false;
+        }
+        else
+        {
+            reg_phoneNo.setError(null);
+            reg_phoneNo.setErrorEnabled(false);
+            return true;
+        }
+    }
+    private boolean validatePassword()
+    {
+        String val =  reg_password.getEditText().getText().toString();
+        String passwordVal = "^" +
+                //"(?=.*[0-9])" +         //at least 1 digit
+                //"(?=.*[a-z])" +         //at least 1 lower case letter
+                //"(?=.*[A-Z])" +         //at least 1 upper case letter
+                "(?=.*[a-zA-Z])" +      //any letter
+                "(?=.*[@#$%^&+=])" +    //at least 1 special character
+                "(?=\\S+$)" +           //no white spaces
+                ".{4,}" +               //at least 4 characters
+                "$";
+
+        if(val.isEmpty())
+        {
+            reg_password.setError("This field cannot be left empty");
+            return false;
+        }
+        else if (!val.matches(passwordVal))
+        {
+            reg_password.setError("Password is too weak");
+            return false;
+        }
+        else
+        {
+            reg_password.setError(null);
+            reg_password.setErrorEnabled(false);
+            return true;
+        }
+    }
+}
